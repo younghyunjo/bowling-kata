8fb70c1d3276b3afe9a63bc2a5b2d25993226eb5
Younghyun Jo <younghyunjo99@gmail.com>
2018-07-30 19:31:27 +0900

    Testcase added

--- tests/unit_tests/Bowling.cpp
+++ tests/unit_tests/Bowling.cpp
@@ -7,7 +7,7 @@
 #include "gtest/gtest.h"
 #include "Bowling.h"
 
-TEST(BOWLING, play) {
+TEST(BOWLING, perfect) {
     Bowling game;
 
     std::vector<std::vector<int>> pinFalls;
@@ -21,3 +21,58 @@ TEST(BOWLING, play) {
     EXPECT_EQ(300, score);
 }
 
+TEST(BOWLING, zero) {
+    Bowling game;
+
+    std::vector<std::vector<int>> pinFalls;
+    for (int i=0; i<10 ;i++)
+        pinFalls.push_back({0});
+
+    game.play(pinFalls);
+    int score = game.getScore();
+
+    EXPECT_EQ(0, score);
+}
+
+TEST(BOWLING, allThree) {
+    Bowling game;
+
+    std::vector<std::vector<int>> pinFalls;
+    for (int i=0; i<10 ;i++)
+        pinFalls.push_back({3,3});
+
+    game.play(pinFalls);
+    int score = game.getScore();
+
+    EXPECT_EQ(60, score);
+}
+
+TEST(BOWLING, allSparesWith4) {
+    Bowling game;
+
+    std::vector<std::vector<int>> pinFalls;
+    for (int i=0; i<9 ;i++)
+        pinFalls.push_back({4,6});
+
+    pinFalls.push_back({4,6,4});
+
+    game.play(pinFalls);
+    int score = game.getScore();
+
+    EXPECT_EQ(140, score);
+}
+
+TEST(BOWLING, nineStrikesLastIsGutter) {
+    Bowling game;
+
+    std::vector<std::vector<int>> pinFalls;
+    for (int i=0; i<9 ;i++)
+        pinFalls.push_back({10});
+
+    pinFalls.push_back({0, 0});
+
+    game.play(pinFalls);
+    int score = game.getScore();
+
+    EXPECT_EQ(240, score);
+}
