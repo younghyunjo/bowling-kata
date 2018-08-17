1decc10ff07103df00b8c53aa4ad17bf305c3dc7
Younghyun Jo <younghyunjo99@gmail.com>
2018-08-17 15:51:28 +0900

    Immutable Bowling Game

--- v2/src/Bowling.h
+++ v2/src/Bowling.h
@@ -10,35 +10,45 @@
 #include <memory>
 #include <vector>
 
-#include "Frame.h"
+#include "ImmutableFrame.h"
 
-class Pinfall {
-public:
-    Pinfall(std::vector<int> pinfalls) : m_pinfalls(pinfalls) {}
-
-private:
-    const std::vector<int> m_pinfalls;
-};
 
 class Bowling {
 public:
-    void play(std::vector<std::vector<int>> pins) {
+    Bowling() {
+        for (int i = 0; i < 10; i++)
+            frames[i] = std::make_unique<ImmutableFrame>();
+    }
 
+    void play(std::vector<std::vector<int>> pins) {
         auto combinedPinfalls = pinfallsCombineForSingleFrame(pins);
 
+//        for (int i=0; i<10; i++) {
+//            std::for_each(std::begin(combinedPinfalls[i]), std::end(combinedPinfalls[i]), [](auto i) {
+//                    std::cout << i;
+//            });
+//            std::cout << std::endl;
+//        }
 
-        //TODO
-
+        for (int i=0; i<10; i++) {
+            auto pin = combinedPinfalls[i];
+            for (auto p : pin) {
+                frames[i] = frames[i]->roll(p);
+            }
+        }
     }
 
     int score() {
-        //TODO
-        return 10;
+        return std::accumulate(std::begin(frames), std::end(frames), 0, [](int& sum, std::unique_ptr<ImmutableFrame>& f) {
+            return sum += f->score();
+        });
     }
 
 private:
-    std::vector<std::vector<std::vector<int>>> pinfallsCombineForSingleFrame(std::vector<std::vector<int>>& pins) {
-        std::vector<std::vector<std::vector<int>>> ret;
+    std::unique_ptr<ImmutableFrame> frames[10];
+
+    std::vector<std::vector<int>> pinfallsCombineForSingleFrame(std::vector<std::vector<int>>& pins) {
+        std::vector<std::vector<int>> ret;
 
         for (auto it = std::begin(pins); it < std::end(pins); it++) {
             int n = 3;
@@ -47,24 +57,14 @@ private:
             else if (it == std::end(pins) -1)
                 n = 1;
 
-            ret.push_back(std::vector<std::vector<int>>{it, it+n});
-        }
-
-        /*
-        std::for_each(std::begin(ret), std::end(ret), [](auto b) {
-            std::cout << "size:" << b.size() << std::endl;
-            std::for_each(std::begin(b), std::end(b), [](auto c) {
-                std::for_each(std::begin(c), std::end(c), [](auto d) {
-                    std::cout << d << std::endl;
-                });
-                std::cout << std::endl;
+            std::vector<int> pinfallsForSingleFrame;
+            std::for_each(it, it+n, [&pinfallsForSingleFrame](auto p) {
+                pinfallsForSingleFrame.insert(pinfallsForSingleFrame.end(), p.begin(), p.end());
             });
-            std::cout << std::endl;
-        });
-        */
-
+            pinfallsForSingleFrame.resize(3);
+            ret.push_back(pinfallsForSingleFrame);
+        }
         return ret;
-
     }
 };
 
--- v2/tests/Bowling.cpp
+++ v2/tests/Bowling.cpp
@@ -14,26 +14,48 @@
 class BowlingTest : public ::testing::Test {
 protected:
     void SetUp() override {
-        /*
-        std::for_each(std::begin(singlePins), std::end(singlePins), [] (std::vector<int> pins){
-            std::for_each(std::begin(pins), std::end(pins), [](int p) {
-                std::cout << p << std::endl;
-            });
-            std::cout << std::endl;
-        });
-        */
     }
 
     void TearDown() override {
     }
 
     const std::vector<std::vector<int>> singlePins{10, std::vector<int>{1, 0}};
-    std::unique_ptr<Bowling> bowling;
+    Bowling bowling;
 };
 
 TEST_F(BowlingTest, Bowling) {
-    bowling->play(singlePins);
-    int score = bowling->score();
+    bowling.play(singlePins);
+    int score = bowling.score();
 
     EXPECT_EQ(10, score);
 }
+
+TEST_F(BowlingTest, Perfect) {
+    std::vector<std::vector<int>> perfectPins{9, std::vector<int>{10}};
+    perfectPins.push_back(std::vector<int>{10, 10, 10});
+
+    bowling.play(perfectPins);
+    int score = bowling.score();
+
+    EXPECT_EQ(300, score);
+}
+
+TEST_F(BowlingTest, Spares) {
+    std::vector<std::vector<int>> perfectPins{9, std::vector<int>{9, 1}};
+    perfectPins.push_back(std::vector<int>{9, 0});
+
+    bowling.play(perfectPins);
+    int score = bowling.score();
+
+    EXPECT_EQ(180, score);
+}
+
+TEST_F(BowlingTest, Zero) {
+    std::vector<std::vector<int>> perfectPins{10, std::vector<int>{0, 0}};
+
+    bowling.play(perfectPins);
+    int score = bowling.score();
+
+    EXPECT_EQ(0, score);
+}
+
